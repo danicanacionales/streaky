5c39bc48fddbc484e91fe87c8646f3e7f156a241
danica nacionales <danicanacionales@gmail.com>
2019-04-30 14:35:03 +0800

    contrib
    april 30, 2019

--- scripts/graph.js
+++ scripts/graph.js
@@ -1,7 +1,5 @@
-// Add squares
-
 const squares = document.querySelector('.squares');
-for (var i = 1; i < 61; i++) {
+for (var i = 1; i < 365; i++) {
   const level = Math.floor(Math.random() * 3);  
   squares.insertAdjacentHTML('beforeend', `<li data-level="${level}"></li>`);
 }
