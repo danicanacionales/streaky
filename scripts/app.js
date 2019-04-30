1dbef05252ae7dc877c8f11c8a171b87bc0392ab
danica nacionales <danicanacionales@gmail.com>
2019-05-01 00:52:56 +0800

    add goal
    may 1, 2019

--- scripts/app.js
+++ scripts/app.js
@@ -3,31 +3,46 @@ const UInewGoalInput = document.getElementById('new-goal-input');
 const UIlengthInput = document.getElementById('length-input');
 const UIgoal = document.getElementById('goal');
 const UIlength = document.getElementById('length');
+const UIgraph = document.getElementById('graph');
 
 class Goal {
-    constructor(title, length){
+    constructor(title, endDate){
         this.title = title;
-        this.length = length;
+        this.startDate = new Date();
+        this.endDate = endDate;
+    }
+
+    static calculateDaysBetween(startDate, endDate){
+        var one_day = 1000 * 60 * 60 * 24;
+        var startDate_ms = startDate.getTime();
+        var endDate_ms = endDate.getTime();
+        var difference_ms = endDate_ms - startDate_ms;
+        return Math.round(difference_ms/one_day);
     }
 }
 
 class UI {
     addGoal(goal){
         UIgoal.textContent = goal.title;
+        goal.length = Goal.calculateDaysBetween(goal.startDate, goal.endDate);
         UIlength.innerHTML = `<i class="fas fa-bolt"></i> ${goal.length} days`;
+        Graph.loadGraph(goal);
     }
 }
 
+document.addEventListener('DOMContentLoaded', Graph.loadGraph);
+
 UInewGoalForm.addEventListener('submit', function(e){
     const title = UInewGoalInput.value;
-    const length = parseInt(UIlengthInput.value);
+    const endDate = new Date(UIlengthInput.value);
 
-    const goal = new Goal(title, length);
+    const goal = new Goal(title, endDate);
     const ui = new UI();
 
-    if(title === '' || length <= 0){
+    if(title === ''){
         console.log('Inputs are invalid');
     }else{
+        // goal.startDate = new Date();
         ui.addGoal(goal);
     }
 
--- scripts/graph.js
+++ scripts/graph.js
@@ -1,5 +1,28 @@
 const squares = document.querySelector('.squares');
-for (var i = 1; i < 365; i++) {
-  const level = Math.floor(Math.random() * 3);  
-  squares.insertAdjacentHTML('beforeend', `<li data-level="${level}"></li>`);
+const months = document.querySelector('.months');
+
+class Graph {
+    static loadGraph(goal){
+        if(goal !== null){
+            console.log(goal);
+            months.setAttribute('grid-template-rows', 
+                `calc(var(--week-width) * 4) /* Jan */
+                calc(var(--week-width) * 4) /* Feb */
+                calc(var(--week-width) * 4) /* Mar */
+                calc(var(--week-width) * 5) /* Apr */
+                calc(var(--week-width) * 4) /* May */
+                calc(var(--week-width) * 4) /* Jun */
+                calc(var(--week-width) * 5) /* Jul */
+                calc(var(--week-width) * 4) /* Aug */
+                calc(var(--week-width) * 4) /* Sep */
+                calc(var(--week-width) * 5) /* Oct */
+                calc(var(--week-width) * 4) /* Nov */
+                calc(var(--week-width) * 5) /* Dec */;`);
+            for (var i = 1; i <= goal.length; i++) {
+              const level = Math.floor(Math.random() * 3);  
+              squares.insertAdjacentHTML('beforeend', `<li data-level="1"></li>`);
+            }
+        }
+        
+    }
 }
