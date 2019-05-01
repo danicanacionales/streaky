17bb766eae21ccf0f0defaacd5cb3a9cfe892aa1
danica nacionales <danicanacionales@gmail.com>
2019-05-01 23:47:53 +0800

    test
    may 1, 2019

--- scripts/graph.js
+++ scripts/graph.js
@@ -6,18 +6,8 @@ class Graph {
         if(goal !== null){
             console.log(goal);
             months.setAttribute('grid-template-rows', 
-                `calc(var(--week-width) * 4) /* Jan */
-                calc(var(--week-width) * 4) /* Feb */
-                calc(var(--week-width) * 4) /* Mar */
-                calc(var(--week-width) * 5) /* Apr */
-                calc(var(--week-width) * 4) /* May */
-                calc(var(--week-width) * 4) /* Jun */
-                calc(var(--week-width) * 5) /* Jul */
-                calc(var(--week-width) * 4) /* Aug */
-                calc(var(--week-width) * 4) /* Sep */
-                calc(var(--week-width) * 5) /* Oct */
-                calc(var(--week-width) * 4) /* Nov */
-                calc(var(--week-width) * 5) /* Dec */;`);
+                `calc(var(--week-width) * 4) /* May */
+                ;`);
             for (var i = 1; i <= goal.length; i++) {
               const level = Math.floor(Math.random() * 3);  
               squares.insertAdjacentHTML('beforeend', `<li data-level="1"></li>`);
